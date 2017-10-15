e1648f60c4249cf0d443f9dbbcfd78bb92e77274
Daniel Hess <dan9186@gmail.com>
2017-10-15 01:26:03 -0700

    using package name instead

--- example_exported_test.go
+++ example_exported_test.go
@@ -1,16 +1,16 @@
 package ledger_test
 
 import (
-	log "github.com/gomicro/ledger"
+	"github.com/gomicro/ledger"
 )
 
-var lvl = log.InfoLevel
+var lvl = ledger.InfoLevel
 
 // ExampleExported demonstrates the default exported logger
 func Example_exported() {
-	log.Debug("We've got a debug line here")
-	log.Info("We've got a info line here")
-	log.Warn("We've got a warn line here")
-	log.Error("We've got a error line here")
-	log.Fatal("We've got a fatal line here")
+	ledger.Debug("We've got a debug line here")
+	ledger.Info("We've got a info line here")
+	ledger.Warn("We've got a warn line here")
+	ledger.Error("We've got a error line here")
+	ledger.Fatal("We've got a fatal line here")
 }
